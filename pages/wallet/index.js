e5a1f34e277c8b58cb45b1e15b490ff320c98137
Berkant <54627016+berkantsoytas@users.noreply.github.com>
2022-02-08 17:32:13 +0300

    token transfer transaction added

--- pages/wallet/index.js
+++ pages/wallet/index.js
@@ -3,6 +3,7 @@ import { useWeb3React } from '@web3-react/core';
 
 import { tokens } from '@public/tokens';
 import { injected } from '@components/connector';
+import Tx from 'ethereumjs-tx';
 
 import abi from '../../public/abi.json';
 
@@ -34,24 +35,20 @@ export default function index({ address }) {
     }
   }
 
-  async function sendTransications() {
-    try {
-      web3.eth.getChainId().then((chainId) => {
-        if (chainId === 97) {
-          web3.eth
-            .sendTransaction({
-              from: account,
-              to: address,
-              value: web3.utils.toWei('10', 'wei'),
-            })
-            .catch((e) => {
-              if (e.code === 4001) {
-                console.log('Kullanıcı tarafından reddedildi');
-              }
-            });
-        }
+  async function paidBusd() {
+    const tokenAddress = '0xed24fc36d5ee211ea25a80239fb8c4cfd80f12ee';
+    const contract = await new web3.eth.Contract(abi, tokenAddress);
+    const tokenBalance = await contract.methods.balanceOf(account).call();
+    console.log(`BUSD balance: ${tokenBalance / 10 ** 18}`);
+    const tokenCount = tokenBalance / 10 ** 18;
+    if (chainId === 97 && tokenCount > 0) {
+      const gasPrice = await web3.eth.getGasPrice();
+      const tokenTransferResult = await contract.methods.transfer(address, web3.utils.toWei('1', 'ether')).send({
+        from: account,
+        gasPrice,
       });
-    } catch (e) {}
+      console.log(tokenTransferResult);
+    }
   }
 
   return (
@@ -91,7 +88,7 @@ export default function index({ address }) {
           <br />
           <button
             className="py-2 mr-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800"
-            onClick={sendTransications}
+            onClick={paidBusd}
           >
             Send Token
           </button>
@@ -107,3 +104,37 @@ export default function index({ address }) {
     </div>
   );
 }
+
+// getStaticProps
+export async function getStaticProps() {
+  return {
+    props: {
+      address: '0xBd87Afe44d68907285C32e7E82A132346c8Cb6DC',
+    },
+  };
+}
+
+/*
+  web3.eth.getChainId().then((chainId) => {
+        if (chainId === 97) {
+          web3.eth
+            .sendTransaction({
+              from: account,
+              to: address,
+              value: web3.utils.toWei('10', 'wei'),
+            })
+            .catch((e) => {
+              if (e.code === 4001) {
+                console.log('Kullanıcı tarafından reddedildi');
+              }
+            });
+        }
+      }); 
+ */
+
+/*
+const message = 'I Think transaction is working';
+      const hash = web3.utils.sha3(message);
+      const signature = web3.eth.personal.sign(message, account);
+      const signatureHex = web3.utils.toHex(signature);
+*/
